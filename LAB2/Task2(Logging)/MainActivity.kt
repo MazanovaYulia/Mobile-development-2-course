6e2ae9ecb861a98efbef1bdab9ede9d50fd44faf
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2022-10-13 02:33:55 +0700

    Update MainActivity.kt

--- LAB2/Task2(Logging)/MainActivity.kt
+++ LAB2/Task2(Logging)/MainActivity.kt
@@ -1,1 +1,26 @@
+package com.example.logging
 
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val showText = Toast.makeText(this,editText.text, Toast.LENGTH_LONG).show()
+        val buttonLog = findViewById<Button>(R.id.button_log)
+        val buttonTimber = findViewById<Button>(R.id.button_timber)
+
+        buttonLog.setOnClickListener{
+            val TAG_LOG = "From EditText"
+            Log.v(TAG_LOG, showText.toString())
+        }
+      //  buttonTimber.setOnClickListener()
+    }
+}
