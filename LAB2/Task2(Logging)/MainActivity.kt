d400953a7299ac4db09e64121aa592b46055afb9
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2022-11-03 14:38:28 +0700

    Update MainActivity.kt

--- LAB2/Task2(Logging)/MainActivity.kt
+++ LAB2/Task2(Logging)/MainActivity.kt
@@ -8,28 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
-    val TAG_LOG = "From EditText"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if(BuildConfig.DEBUG){
-            Timber.plant(Timber.DebugTree())
-        }
 
         val editText = findViewById<EditText>(R.id.edit_text)
         val buttonLog = findViewById<Button>(R.id.button_log)
         val buttonTimber = findViewById<Button>(R.id.button_timber)
 
         buttonLog.setOnClickListener{
-            Log.v(TAG_LOG, editText.text.toString())
+            val tag = "From EditText"
+            Log.v(tag, editText.text.toString())
         }
+        Timber.plant(Timber.DebugTree())
         buttonTimber.setOnClickListener{
             Timber.v(editText.text.toString())
         }
-
-
     }
 }
