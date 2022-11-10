9a8360a99943cb29343f08ad942f99d0ee0e5126
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2022-11-10 15:01:08 +0700

    Update MainActivity.kt

--- LAB3/Task2(NestedLayouts_2)/MainActivity.kt
+++ LAB3/Task2(NestedLayouts_2)/MainActivity.kt
@@ -9,12 +9,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var clickCounter : Int = 0
         setContentView(R.layout.activity_main)
         val buttonRoll = (findViewById<Button>(R.id.button_roll))
-        buttonRoll.setOnClickListener { counter() }
+        buttonRoll.setOnClickListener {
+            counter(clickCounter)
+            clickCounter += 1
+        }
     }
 
-        private fun counter() {
+    private fun counter(clickCounter: Int) {
         val text11 = (findViewById<TextView>(R.id.text11))
         val text12 = (findViewById<TextView>(R.id.text12))
         val text13 = (findViewById<TextView>(R.id.text13))
@@ -29,34 +33,33 @@ class MainActivity : AppCompatActivity() {
         val intText12 = text12.text.toString().toIntOrNull()
         val intText13 = text13.text.toString().toIntOrNull()
 
-          fun addOne(x: Int) = x + 1
+        fun addOne(x: Int) = x + 1
 
-            if (intText11 != null) {
-                val count = addOne(intText11).toString()
-                text12.text = count
-                text11.text = ""
-                text22.text = count
-                text21.text = ""
-                text32.text = count
-                text31.text = ""
-            }
-            if (intText12 != null) {
-                val count = addOne(intText12).toString()
-                text13.text = count
-                text12.text = ""
-                text23.text = count
-                text22.text = ""
-                text33.text = count
-                text32.text = ""
-            }
-            if (intText13 != null) {
-                val count = addOne(intText13).toString()
-                text11.text = count
-                text13.text = ""
-                text21.text = count
-                text23.text = ""
-                text31.text = count
-                text33.text = ""
-            }
+        if (clickCounter == 1) {
+            text12.text = clickCounter.toString()
+            text11.text = ""
+            text22.text = clickCounter.toString()
+            text21.text = ""
+            text32.text = clickCounter.toString()
+            text31.text = ""
         }
+        if (clickCounter == 2) {
+            val count = addOne(intText12).toString()
+            text13.text = count
+            text12.text = ""
+            text23.text = count
+            text22.text = ""
+            text33.text = count
+            text32.text = ""
+        }
+        if (clickCounter == 3) {
+            val count = addOne(intText13).toString()
+            text11.text = count
+            text13.text = ""
+            text21.text = count
+            text23.text = ""
+            text31.text = count
+            text33.text = ""
+        }
+    }
 }
