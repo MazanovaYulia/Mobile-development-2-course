2a494c0318f7de7d3712ff10d561b6dfeea37cc7
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2022-11-14 12:56:29 +0700

    Update MainActivity.kt

--- LAB3/Task2(NestedLayouts_2)/MainActivity.kt
+++ LAB3/Task2(NestedLayouts_2)/MainActivity.kt
@@ -29,38 +29,14 @@ class MainActivity : AppCompatActivity() {
         val text32 = (findViewById<TextView>(R.id.text32))
         val text33 = (findViewById<TextView>(R.id.text33))
 
-        if (clickCounter % 3 == 2) {
-            text11.text = ""
-            text12.text = clickCounter.toString()
-            text13.text = ""
-            text21.text = ""
-            text22.text = clickCounter.toString()
-            text23.text = ""
-            text31.text = ""
-            text32.text = clickCounter.toString()
-            text33.text = ""
-        }
-        if (clickCounter % 3 == 0) {
-            text11.text = ""
-            text12.text = ""
-            text13.text = clickCounter.toString()
-            text21.text = ""
-            text22.text = ""
-            text23.text = clickCounter.toString()
-            text31.text = ""
-            text32.text = ""
-            text33.text = clickCounter.toString()
-        }
-        if (clickCounter % 3 == 1) {
-            text11.text = clickCounter.toString()
-            text12.text = ""
-            text13.text = ""
-            text21.text = clickCounter.toString()
-            text22.text = ""
-            text23.text = ""
-            text31.text = clickCounter.toString()
-            text32.text = ""
-            text33.text = ""
-        }
+        text11.text = if (clickCounter % 3 == 1) clickCounter.toString() else ""
+        text12.text = if (clickCounter % 3 == 2) clickCounter.toString() else ""
+        text13.text = if (clickCounter % 3 == 0) clickCounter.toString() else ""
+        text21.text = if (clickCounter % 3 == 1) clickCounter.toString() else ""
+        text22.text = if (clickCounter % 3 == 2) clickCounter.toString() else ""
+        text23.text = if (clickCounter % 3 == 0) clickCounter.toString() else ""
+        text31.text = if (clickCounter % 3 == 1) clickCounter.toString() else ""
+        text32.text = if (clickCounter % 3 == 2) clickCounter.toString() else ""
+        text33.text = if (clickCounter % 3 == 0) clickCounter.toString() else ""
     }
 }
