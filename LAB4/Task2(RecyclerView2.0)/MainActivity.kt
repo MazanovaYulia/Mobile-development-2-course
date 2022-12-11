5e9d572e5a5c8854484442825859ec488b0ac5f2
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2022-12-12 00:43:24 +0700

    Update MainActivity.kt

--- LAB4/Task2(RecyclerView2.0)/MainActivity.kt
+++ LAB4/Task2(RecyclerView2.0)/MainActivity.kt
@@ -1,1 +1,35 @@
+package com.example.recyclerview
 
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.rView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = Adapter(this, fetchList(), this)
+    }
+
+    class ColorData(val colorName: String, val colorHex: Int)
+
+    private fun fetchList(): ArrayList<ColorData> {
+        return arrayListOf(
+            ColorData("WHITE", Color.WHITE),
+            ColorData("BLACK", Color.BLACK),
+            ColorData("BLUE", Color.BLUE),
+            ColorData("RED", Color.RED),
+            ColorData("MAGENTA", Color.MAGENTA)
+        )
+    }
+
+    fun onCellClickListener(data: ColorData) {
+        Toast.makeText(this, "IT’S " + data.colorName, Toast.LENGTH_SHORT).show()
+    }
+}
