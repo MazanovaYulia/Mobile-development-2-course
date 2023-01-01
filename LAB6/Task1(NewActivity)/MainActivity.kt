d6d38689dc9fe0c62844eabd1583e37ed2d76020
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2023-01-02 05:06:08 +0700

    Update MainActivity.kt

--- LAB6/Task1(NewActivity)/MainActivity.kt
+++ LAB6/Task1(NewActivity)/MainActivity.kt
@@ -1,1 +1,22 @@
+package com.example.newactivity
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btn_show_pic)
+
+        button.setOnClickListener {
+            intent = Intent(this, PicActivity::class.java)
+            intent.putExtra("picLink", "https://www.rosphoto.com/images/u/articles/1510/4_8.jpg")
+            startActivity(intent)
+        }
+    }
+}
