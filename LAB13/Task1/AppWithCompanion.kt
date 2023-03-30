2d62f9e32a9b95c64c9c5a50868facba2c8f1323
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2023-03-30 18:03:06 +0700

    Create AppWithCompanion.kt

---
+++ LAB13/Task1/AppWithCompanion.kt
@@ -0,0 +1,16 @@
+package com.example.rickandmorty
+
+import android.app.Application
+
+class AppWithCompanion: Application(){
+    companion object {
+        lateinit var charApi: RetrofitCharServices
+        lateinit var epApi: RetrofitEpServices
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        charApi = Common.retrofitCharService
+        epApi = Common.retrofitEpService
+    }
+}
